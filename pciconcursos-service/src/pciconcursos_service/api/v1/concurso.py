3aef1e81248f475925a3ea56114bb977a27b0778
kavvata <gabriel.kawata@gmail.com>
2025-11-16 21:45:57 -0300

    build(service): moved root to a service directory

--- pciconcursos-service/src/pciconcursos_service/api/v1/concurso.py
+++ pciconcursos-service/src/pciconcursos_service/api/v1/concurso.py
@@ -3,12 +3,13 @@ import typing as t
 from fastapi import APIRouter, Depends, HTTPException
 
 from pciconcursos_service.dependencies import concurso_service
+from pciconcursos_service.domain.concursos.entity import Concurso
 from pciconcursos_service.domain.concursos.service import ConcursoService
 
 router = APIRouter(prefix="/api/v1/concurso")
 
 
-@router.get("/")
+@router.get("/", response_model=list[Concurso])
 async def get_concursos_ativos(service: t.Annotated[ConcursoService, Depends(concurso_service)]):
     concursos = await service.get_concursos_ativos()
     if concursos is None:
