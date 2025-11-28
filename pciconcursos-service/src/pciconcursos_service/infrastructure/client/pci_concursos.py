ab299dfbec6d3118a34438e1fd11a5e68c7c0673
kavvata <gabriel.kawata@gmail.com>
2025-11-28 13:25:33 -0300

    feat(scrape): collected nivel and area de atuacao from scraped items

--- pciconcursos-service/src/pciconcursos_service/infrastructure/client/pci_concursos.py
+++ pciconcursos-service/src/pciconcursos_service/infrastructure/client/pci_concursos.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import aiohttp
 import structlog
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from structlog.stdlib import BoundLogger
 
 from pciconcursos_service.domain.concursos.entity import Concurso
@@ -17,7 +17,7 @@ class PciConcursosClient(ConcursoClient):
         self.link = link
         self.region_config = region_config
 
-    async def get_concursos_ativos(self, region):
+    async def get_concursos_ativos(self, region) -> list[Concurso]:
         async with aiohttp.ClientSession() as session:
             async with session.get(self.link) as response:
                 soup = BeautifulSoup(await response.text(), "html.parser")
@@ -40,12 +40,20 @@ class PciConcursosClient(ConcursoClient):
             name = line.find("a").text.strip()
             link = line.find("a", href=True)["href"]
 
-            cd_content = str(line.find(class_="cd"))
-            ce_content = str(line.find(class_="ce"))
+            cd_soup: Tag = line.find(class_="cd")
+            ce_soup = line.find(class_="ce")
+            cd_content = str(cd_soup)
+            ce_content = str(ce_soup)
+
+            cd_span = cd_soup.find("span")
+            area_str, nivel_str = list(cd_span._all_strings())[:2]
 
             vagas = "".join(re.findall(r"(\d*) vaga", cd_content))
-            nivel = "/".join(re.findall(r"Superior|Médio|Técnico|Fundamental", cd_content))
+            nivel = "/".join([a.strip() for a in nivel_str.split("-")])
             salario = "".join(re.findall(r"R\$ *\d*\.*\d*\,*\d*", cd_content))
+            area_atuacao_list = [a.strip() for a in area_str.split(",") if a]
+
+            await self.log.ainfo("found list", area_list=area_atuacao_list, nivel=nivel)
 
             regiao = line.parent.find_previous("div", class_="uf").text
 
@@ -62,6 +70,7 @@ class PciConcursosClient(ConcursoClient):
                     nome=name,
                     regiao=regiao,
                     vagas=int(vagas) if vagas else None,
+                    area_atuacao=area_atuacao_list,
                     nivel=nivel,
                     salario_max=salario or None,
                     inscricao_ate=datetime.strptime(inscricao, "%d/%m/%Y") if inscricao else None,
